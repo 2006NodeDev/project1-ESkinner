90a2a99a515b6363c59143315d0a1dc3a919420f
benjamin skinner <benjamin.skinner15@gmail.com>
2020-07-17 10:10:37 -0400

    update for first GCP push

--- express-server/src/middleware/cors-filter.ts
+++ express-server/src/middleware/cors-filter.ts
@@ -2,8 +2,8 @@ import { Request, Response, NextFunction } from "express";
 
 export function corsFilter(req:Request, res:Response, next:NextFunction){
     //we should always, on every request, we have to set the Access-Control-Allow-Origin header
-    res.header('Access-Control-Allow-Origin', String(req.originalUrl)) //this is bad. don't do this
-    res.header('Access-Control-Allow-Origin', 'Origin, Content-Type, Accept')
+    res.header('Access-Control-Allow-Origin', `${req.headers.origin}`)//this is a dirty hack, its really bad, don't do it when you app is deployed or I will be very disappointed in you
+    res.header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept')
     res.header('Access-Control-Allow-Credentials', 'true')
     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE')
     if(req.method ==='OPTIONS'){
